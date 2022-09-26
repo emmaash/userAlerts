9c19c475239c9fc2af031c60cd0715b4191b32f0
Emma <emersonpaige5@gmail.com>
2022-09-26 10:05:29 -0400

    changed the sweet alert

--- app/app.js
+++ app/app.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
 $(window).on("load", function (){
 console.log("loaded");
 // $(".modal").css("display", "none");
-// Swal.fire('You Rock!')
-Swal.fire({
-    title: 'You Rock!',
-    text: 'Modal with a custom image.',
-    imageUrl: 'https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-640x514.jpg',
-    imageWidth: 300,
-    imageHeight: 200,
-    imageAlt: 'Custom image',
-  })
+Swal.fire('You Rock!')
+// Swal.fire({
+//     title: 'You Rock!',
+//     text: 'Modal with a custom image.',
+//     imageUrl: 'https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-640x514.jpg',
+//     imageWidth: 300,
+//     imageHeight: 200,
+//     imageAlt: 'Custom image',
+//   })
 
 });
 
