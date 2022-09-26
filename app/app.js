f5b8dea105da3d2cbc31a6e8b0bad0688b64bd3f
Emma <emersonpaige5@gmail.com>
2022-09-26 10:03:17 -0400

    intial Commit for User Feedback

---
+++ app/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    console.log("ready");
+});
+
+$(window).on("load", function (){
+console.log("loaded");
+// $(".modal").css("display", "none");
+// Swal.fire('You Rock!')
+Swal.fire({
+    title: 'You Rock!',
+    text: 'Modal with a custom image.',
+    imageUrl: 'https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-640x514.jpg',
+    imageWidth: 300,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+
+});
+
+// function fire(paramObj: Object){
+
+// }
